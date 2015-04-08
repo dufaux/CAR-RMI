20060b2515955d56743e4f3096643f4a7b5fb3ff
Rakotoarivony Allan-noel <rakotoarivony@a10p9.fil.univ-lille1.fr>
2015-04-08 14:02:19 +0200

    gitigonre added

--- src/main/MainCreateSiteTree.java
+++ src/main/MainCreateSiteTree.java
@@ -18,22 +18,34 @@ public class MainCreateSiteTree {
 	 * @throws AlreadyBoundException 
 	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) throws RemoteException, AlreadyBoundException, UnknownHostException {
+	public static void main(String[] args){
 		
 		InetAddress addr;
-	    addr = InetAddress.getLocalHost();
-	    String hostname = addr.getHostName();
+	    try {
+			addr = InetAddress.getLocalHost();
+			String hostname = addr.getHostName();
+		    
+			System.setProperty("java.rmi.server.hostname",hostname);
+			LocateRegistry.createRegistry(1099);
+			
+			SiteTree site = new SiteTreeImpl(hostname);
+			String rmiadd = "site";
+			
+			Registry reglocal = LocateRegistry.getRegistry("localhost");
+			reglocal.bind(rmiadd, site);
+			System.out.println("NEW SITE CREATED ON THIS COMPUTER : "+hostname+" \n" +
+					"bound on local registry with this adress : "+rmiadd);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    
-		System.setProperty("java.rmi.server.hostname",hostname);
-		LocateRegistry.createRegistry(1099);
-		
-		SiteTree site = new SiteTreeImpl(hostname);
-		String rmiadd = "site";
-		
-		Registry reglocal = LocateRegistry.getRegistry("localhost");
-		reglocal.bind(rmiadd, site);
-		System.out.println("NEW SITE CREATED ON THIS COMPUTER : "+hostname+" \n" +
-				"bound on local registry with this adress : "+rmiadd);
 	}
 
 }
