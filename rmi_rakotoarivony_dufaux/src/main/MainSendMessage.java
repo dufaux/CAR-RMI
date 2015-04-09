43516af077022833d40599c59e4286012e56fa88
lan21 <allan.rakotoarivony@gmail.com>
2015-04-09 10:01:18 +0200

    synchronize + timestamp

---
+++ rmi_rakotoarivony_dufaux/src/main/MainSendMessage.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import rmi.Message;
+import rmi.Site;
+
+/**
+ * Send the message from the site on the computer as params.
+ *
+ */
+public class MainSendMessage {
+
+	/**
+	 * @param args
+	 * @throws RemoteException 
+	 * @throws NotBoundException 
+	 */
+	public static void main(String[] args) throws RemoteException, NotBoundException {
+		if (args.length<2){
+			usage();
+			return;
+		}
+		Registry reg = LocateRegistry.getRegistry(args[0]);
+		String numero = args[1];
+		Site site = (Site) reg.lookup("site"+numero);
+		Message m = site.createMessage("Coucou c'est "+args[0]);
+		site.sendMessage(m);
+	}
+	
+	private static void usage(){
+		System.out.println("usage : sendMessage <pc_name> <machine_id>");
+		System.out.println("\t<pc_name> : name or adress of a pc");
+		System.out.println("\t<machine_id> : number of the VM on the pc");
+		
+	}
+
+}
